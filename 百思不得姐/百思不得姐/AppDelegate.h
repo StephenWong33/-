f2745c96505efb21ad6eca4e89b8c1c826fd27d9
StephenWong33 <1030751708@qq.com>
2016-04-12 20:28:02 +0800

    创建百思不得姐

---
+++ 百思不得姐/百思不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  百思不得姐
+//
+//  Created by 王亚帅 on 16/4/12.
+//  Copyright © 2016年 Wong Stethen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
